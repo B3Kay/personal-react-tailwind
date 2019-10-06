b2dc746053e2bd9d1be7a81344cf83838d3add05
Benjamin Karlsson <benjaminka@hiqgbgl0672.local>
2019-10-06 17:09:16 +0200

    use grid for color layot grid

--- src/pages/Second.jsx
+++ src/pages/Second.jsx
@@ -1,17 +1,16 @@
-import React from "react";
+// /** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import React, {useState} from "react";
 
 import { fullConfig } from "../tailwindConfig";
-// const gradients = fullConfig.linearGradients.color
+
+
+const tailwindGradients = fullConfig.theme.linearGradients.colors;
 
 const SecondPage = () => {
-  const colorTest = [
-    { color: "indigo-pink" },
-    { color: "indigo-blue" },
-    { color: "indigo-purple" }
-  ];
-  console.log(fullConfig.theme.linearGradients.colors);
+  const [theme, setTheme] = useState('indigo-pink');
 
-  const tailwindGradients = fullConfig.theme.linearGradients.colors;
+
 
   return (
     <div
@@ -19,23 +18,34 @@ const SecondPage = () => {
       // style={{ height: "100vh" }}
     >
       <div className="container  m-auto ">
-        <h2 className="text-gray-800 text-3xl font-bold">Choose a color theme</h2>
-        <div className="flex flex-wrap ">
-          {/* {colorTest.map(({ color }) => (
-          <div
-            key={color}
-            className={`bg-gradient-br-${color} rounded m-2 w-1/6 h-64 text-center`}
-          >
-            <h4 className="text-white">{color}</h4>
-          </div>
-        ))} */}
-
+        <h2 className="text-gray-800 text-3xl font-bold pt-16 pb-3">
+          Choose a color theme
+        </h2>
+        <h3 className="pb-10"><span className={`bg-gradient-br-${theme} text-white px-2 py-1 rounded `}>{theme}</span> is Current theme</h3>
+        <div
+          css={css`
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(16rem, 1fr));
+            grid-gap: 3rem;
+            grid-auto-rows: 20rem;
+          `}
+        >
           {Object.keys(tailwindGradients).map(color => (
-            <div key={color} className={` w-1/4 h-64  py-8 px-10`}>
+            <div key={color}>
               <div
-                className={`bg-gradient-br-${color} rounded  h-full text-center  text-center flex shadow-lg`}
+                className={`bg-gradient-br-${color} rounded  h-full text-center  text-center flex shadow-lg hover:shadow-xl`}
+                css={css`
+                  transition: transform 0.3s, box-shadow 0.3s;
+                  &:hover {
+                    transform: translate(0.1rem, -0.5rem);
+                  }
+                `}
+                onClick={() => setTheme(color)}
               >
-                <h4 className="text-white m-auto text-xl font uppercase tracking-wide">
+                <h4 
+                  className="text-white m-auto text-xl font uppercase tracking-wide"
+                  
+                  >
                   {color}
                 </h4>
               </div>
